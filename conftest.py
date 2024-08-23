f2bc514a68b2e21fd081bf84e0d050ed7016d01b
Adam <adamfgcross@gmail.com>
2024-08-23 12:19:11 -0400

    add mergesort to test parameterization

--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@
 import pytest
 from sortalgorithms.insertion_sort import sort_array as insertion_sort
 from sortalgorithms.bubble_sort import sort_array as bubble_sort
+from sortalgorithms.merge_sort import sort_array as merge_sort
 
 @pytest.fixture(params=[insertion_sort, 
-                        bubble_sort], 
+                        bubble_sort,
+                        merge_sort], 
                 ids = ["insertion_sort",
-                       "bubble_sort"])
+                       "bubble_sort",
+                       "merge_sort"])
 def array_sort_function(request):
     return request.param
